7bd2a8958886d65486cb4ebafb3b2ef044264235
Yuanda <yuanda.dong@innomate.com.au>
2025-07-10 01:33:51 +1000

    [code] enrichment

--- zebar/attaquer-solid-ts/src/CurrentApps/CurrentApps.tsx
+++ zebar/attaquer-solid-ts/src/CurrentApps/CurrentApps.tsx
@@ -11,19 +11,26 @@ const CurrentApps: Component<CurrentAppsProps> = (props) => {
 
 
   return (
-		<div class="template">
-			<For each={props.glazewm?.displayedWorkspace.children}>{(child) => {
-        if (child.type == "window"){
+    <div class="template">
+      <For each={props.glazewm?.displayedWorkspace.children}>{(child) => {
+        if (child.type == "window") {
           const handleAppClick = () => {
             props.glazewm.runCommand(
               `shell-exec %userprofile%/.glzr/zebar/attaquer-solid-ts/dist/assets/scripts/FocusWindow.ahk ${child.handle}`
             );
           };
-          return <div class="window-title" onClick={handleAppClick}>{child.title}</div>;
+          const handleRightClick = () => {
+            props.glazewm.runCommand(
+              `set-minimized`
+            );
+          }
+          return <div class="window-title" onClick={handleAppClick} onContextMenu={handleRightClick}>
+            {child.title.length > 15 ? child.title.slice(0, 5) + '…' : child.title}
+          </div>;
         }
       }}</For>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default CurrentApps;
